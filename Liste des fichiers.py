e5d836f648bc095d74fd37427fc2e8e96932b828
Dany Dubé <74633244+danydube1971@users.noreply.github.com>
2023-02-19 13:53:04 -0500

    Update Liste des fichiers.py

--- Liste des fichiers.py
+++ Liste des fichiers.py
@@ -1,6 +1,10 @@
-"""le script utilise Flask pour créer une application Web qui affiche une liste de fichiers dans le répertoire courant avec leur nom, type, taille et date de modification. Les fichiers sont triés par ordre alphabétique et affichés dans un tableau HTML dans une page Web. 
+"""Le script utilise Flask pour créer une application Web qui affiche une liste de fichiers dans le répertoire courant 
+avec leur nom, type, taille et date de modification. Les fichiers sont triés par ordre alphabétique et affichés dans un 
+tableau d'une page Web. 
 
-Pour utiliser ce script, placer ce script à la racine du dossier des fichiers que vous voulez afficher dans la page web. Créer un dossier nommé templates dans le même dossier que les fichiers à afficher, puis, placez-y le fichier liste_fichiers.html dedans. Exécuter le script dans un terminal avec la commande: python3 Liste des fichiers.py
+Pour utiliser ce script, placer ce script à la racine du dossier des fichiers que vous voulez afficher dans la page web. 
+Créer un dossier nommé templates dans le même dossier que les fichiers à afficher, puis, placez-y le fichier liste_fichiers.html dedans. 
+Exécuter le script dans un terminal avec la commande: python3 Liste des fichiers.py
 """
 
 import os
